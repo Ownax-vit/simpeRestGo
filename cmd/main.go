61eeb8164367fef74b143f9f2ed901bd100b10d4
Ownax-vit <ownax@mail.ru>
2023-03-02 18:35:01 +0300

    init routes api

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/Ownax-vit/simpeRestGo"
+	"github.com/Ownax-vit/simpeRestGo/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
